89d8fd1b9e4ad9eeb858ec5ae7760f3c274f664d
Shatohin <shatohin.aleksei@gmail.com>
2022-04-14 21:46:15 +0300

    обновил куки

--- config/web.php
+++ config/web.php
@@ -14,7 +14,7 @@ $config = [
     'components' => [
         'request' => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
-            'cookieValidationKey' => '',
+            'cookieValidationKey' => '4iWOX1LMiO6lEWJ-WrgqAhi4uAh3bF_q',
         ],
         'cache' => [
             'class' => 'yii\caching\FileCache',
